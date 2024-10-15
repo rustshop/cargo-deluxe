e09a0541f586d37763b579ff3da88e0a87b99ffa
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-10-15 10:17:55 -0700

    feat: CARGO_TARGET_SPECIFIC_ENVS substitution

--- cargo-deluxe/src/bin/cargo.rs
+++ cargo-deluxe/src/bin/cargo.rs
@@ -1,28 +1,32 @@
+use std::env;
 use std::ffi::OsString;
 use std::os::unix::prelude::OsStrExt;
 use std::os::unix::process::CommandExt;
+use std::process::Command;
 
 use bin_intercept::Intercept;
 use cargo_deluxe::init_tracing;
-use color_eyre::eyre::OptionExt;
+use color_eyre::eyre::{Context, OptionExt};
 use color_eyre::Result;
 use tracing::{trace, warn};
 
 const LOG_TARGET: &str = "cargo-deluxe::cargo";
+
+const CARGO_TARGET_SPECIFIC_ENVS_ENV: &str = "CARGO_TARGET_SPECIFIC_ENVS";
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     init_tracing()?;
 
     let exec_res = Intercept::new()?.intercept(|cargo_cmd| -> color_eyre::Result<()> {
-        let org_args: Vec<_> = std::env::args_os().skip(1).collect();
+        let org_args: Vec<_> = env::args_os().skip(1).collect();
         let mut org_args_iter = org_args.clone().into_iter();
 
         let mut new_args = vec![];
         let mut package = None;
         let mut target_dir =
-            std::env::var_os("CARGO_BUILD_TARGET_DIR").unwrap_or_else(|| OsString::from("target"));
-        let mut _target = None;
+            env::var_os("CARGO_BUILD_TARGET_DIR").unwrap_or_else(|| OsString::from("target"));
+        let mut target = None;
         let mut bin = None;
         let mut _subcmd = None;
 
@@ -46,7 +50,7 @@ fn main() -> Result<()> {
                     let val = org_args_iter
                         .next()
                         .ok_or_eyre("Missing argument to {arg}")?;
-                    _target = Some(val.clone());
+                    target = Some(String::from_utf8(val.as_bytes().to_owned())?);
                     new_args.push(arg_os_string);
                     new_args.push(val);
                 }
@@ -85,6 +89,13 @@ fn main() -> Result<()> {
             }
         }
 
+        let target_underscores = match target {
+            Some(t) => t,
+            None => {
+                detect_host_target()?
+            },
+        }.replace('-', "_");
+
         let new_target_dir = match (package, bin) {
             (None, None) => target_dir,
             (None, Some(bin)) => {
@@ -102,10 +113,43 @@ fn main() -> Result<()> {
             ]
             .join(&OsString::from("/")),
         };
+
+        if let Some(env_names_to_sub) = env::var(CARGO_TARGET_SPECIFIC_ENVS_ENV).ok().as_ref().map(|envs| envs.split(',')) {
+            for env_name_to_sub in env_names_to_sub {
+                let env_name_src = env_name_to_sub.replace("target", &target_underscores);
+
+                let env_name_dst = env_name_to_sub.replace("_target", "");
+                if env_name_src == env_name_to_sub {
+                    warn!(
+                        target: LOG_TARGET,
+                        src=env_name_to_sub,
+                        "Target-specific env variable name does not seem to contain `target` string"
+                    );
+                }
+
+                if let Ok(val) = env::var(&env_name_src) {
+                    trace!(
+                        target: LOG_TARGET,
+                        src=env_name_src,
+                        dst=env_name_dst,
+                        val,
+                        "Using target-specific env variable"
+                    );
+                    cargo_cmd.env(env_name_dst, val);
+                } else {
+                    trace!(
+                        target: LOG_TARGET,
+                        src=env_name_src,
+                        "Target-specific env variable not set"
+                    );
+                }
+            }
+        }
         trace!(
             target: LOG_TARGET,
-            target_dir=%String::from_utf8_lossy(new_target_dir.as_bytes()),
             args=?new_args,
+            ?target_underscores,
+            target_dir=%String::from_utf8_lossy(new_target_dir.as_bytes()),
             "Calling next command");
         cargo_cmd.args(new_args);
         cargo_cmd.env("CARGO_BUILD_TARGET_DIR", new_target_dir);
@@ -113,4 +157,16 @@ fn main() -> Result<()> {
     })?.exec();
 
     Err(exec_res)?
+}
+
+fn detect_host_target() -> Result<String> {
+    let out = Command::new("rustc").arg("-vV").output()?;
+
+    let host_line = out
+        .stdout
+        .split(|b| *b == b'\n')
+        .find(|s| s.starts_with(b"host: "))
+        .ok_or_eyre("Could not find `host: ` line in rustc -vV output")?;
+
+    String::from_utf8(host_line[6..].to_vec()).context("Malformed `rustc -vV` output")
 }
